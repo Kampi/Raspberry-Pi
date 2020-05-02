0bc41a43a09d9cd7205ff1c564591c10a297edda
Daniel Kampert <DanielKampert@kampis-elektroecke.de>
2020-05-02 19:17:26 +0200

    Update 'GPS.py'

--- Software/UART/GPS.py
+++ Software/UART/GPS.py
@@ -1,138 +1,36 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-import serial
 import time
+import serial
 
-# Dictionary fuer die empfangene Daten
-Protokolle = {}
+if(__name__ == "__main__"):
+    UART = serial.Serial("COM3", 9600)
 
-# Variablen fuer die Uhrzeit
-Uhrzeit = ""
-Datum = ""
-Stunde = ""
-Minute = ""
-Sekunde = ""
-Tag = ""
-Monat = ""
-Jahr = ""
+    try:
+        if(not(UART.isOpen())):
+            UART.open()
+    except:
+        print("[ERROR] Can not open serial port!")
+        exit()
 
-# Koordinaten
-Koordinaten_Laenge = 0.0
-Koordinaten_Breite = 0.0
-Breitengrad = 0.0
-Laengengrad = 0.0
-Kurs = 0.0
-Winkelminuten_Breite = 0.0
-Winkelminuten_Laenge = 0.0
-Winkelsekunden_Breite = 0.0
-Winkelsekunden_Laenge = 0.0
-Hoehe = 0.0
+    while(True):
+        Datasets = dict()
+        Message = UART.readline().decode("ascii").strip().split(",")
+        if("$" in Message[0]):
+            Datasets[Message[0]] = Message[1:]
 
-# Geschwindigkeit
-Geschwindigkeit_Knoten = 0.0
-Geschwindigkeit_Kilometer = 0.0
-
-# Sonstiges
-Status_GPRMC = ""
-Quali_GPGGA = ""
-Richtung_1_GPRMC = ""
-Richtung_2_GPRMC = ""
-Anzahl_Sat = ""
-
-# UART oeffnen
-UART = serial.Serial("/dev/ttyUSB0", 4800)
-UART.open()
-
-# Startmeldung
-print ""
-print "+---------------------------------------------------------------------+"
-print "| Dieses Programm empfaengt Daten einer Holux GPS-Maus.               |"
-print "| Der empfangene Datensatz wird zerlegt und in der Konsole angezeigt. |"
-print "+---------------------------------------------------------------------+"
-print ""
-
-while True:
-
-	Protokolle = {}
-
-	for Counter in range(3):
-	
-		Daten = []
-	
-		# Zeile einlesen
-		Zeile = UART.readline()
-		Zeile = Zeile.strip()
-
-		# Zeile trennen
-		Daten = Zeile.split(",")
-
-		# Daten dem Dictionary hinzufuegen
-		Schluessel = Daten[0]
-		del Daten[0]
-		Protokolle.update({Schluessel:Daten})
-
-	for Key in Protokolle.iterkeys():
-
-		# Daten extrahieren
-		if(Key == "$GPRMC"):
-
-			# Datum formatieren
-			Uhrzeit = Protokolle[Key][0]
-			Stunde = Uhrzeit[0:2]
-			Minute = Uhrzeit[2:4]
-			Sekunde = Uhrzeit[4:6]
-			Datum = Protokolle[Key][8]
-			Tag = Datum[0:2]
-			Monat = Datum[2:4]
-			Jahr = Datum[4:6]
-
-			# Koordinaten
-			Breitengrad = int(float(Protokolle[Key][2]) / 100.0)
-			Laengengrad = int(float(Protokolle[Key][4]) / 100.0)
-			Winkelminuten_Breite = int(float(Protokolle[Key][2][2:]))
-			Winkelminuten_Laenge = int(float(Protokolle[Key][4][3:]))
-			Winkelsekunden_Breite = float(Protokolle[Key][2][4:]) * 100.0
-			Winkelsekunden_Laenge = float(Protokolle[Key][4][5:]) * 100.0
-
-			# Koordinaten umrechnen
-			Koordinaten_Breite = round((((float(Winkelsekunden_Breite) / 60.0) + float(Winkelminuten_Breite))/60.0) + float(Breitengrad), 6)
-			Koordinaten_Laenge = round((((float(Winkelsekunden_Laenge) / 60.0) + float(Winkelminuten_Laenge))/60.0) + float(Laengengrad), 6)
-
-			# Geschwindigkeit auslesen
-			Geschwindigkeit_Knoten = float(Protokolle[Key][2])
-			Geschwindigkeit_Kilometer = Geschwindigkeit_Knoten * 1.852
-
-			# Sonstiges
-			Status_GPRMC = Protokolle[Key][1]
-			Richtung_1_GPRMC = Protokolle[Key][3]
-			Richtung_2_GPRMC = Protokolle[Key][5]
-
-		elif(Key == "$GPGGA"):
-
-			# Koordinaten
-			Hoehe = float(Protokolle[Key][8])
-
-			# Sonstiges
-			Quali_GPGGA = Protokolle[Key][5]
-			Anzahl_Sat = Protokolle[Key][6]
-
-		else:
-			pass
-
-	# Ausgabe
-	print "Datum: " + Tag + "/" + Monat + "/" + Jahr
-	print "Uhrzeit: " + Stunde + ":" + Minute + ":" + Sekunde
-	print "Status: " + Status_GPRMC
-	print "Qualitaet: " + Quali_GPGGA
-	print "Anzahl Satelliten: " + Anzahl_Sat
-	print "Koordinaten Breite: " + str(Koordinaten_Breite)
-	print "Koordinaten Laenge: " + str(Koordinaten_Laenge)
-	print "Breitengrad: " + Richtung_1_GPRMC + " " + str(Breitengrad) + "°" + str(Winkelminuten_Breite) + "'" + str(Winkelsekunden_Breite) + "''"
-	print "Laengengrad: " + Richtung_2_GPRMC + " " + str(Laengengrad) + "°" + str(Winkelminuten_Laenge) + "'" + str(Winkelsekunden_Laenge) + "''"
-	print "Hoehe vom MSL: " + str(Hoehe)
-	print "Geschwindigkeit: " + str(Geschwindigkeit_Knoten) + " Knoten" + " - " + str(Geschwindigkeit_Kilometer) + " km/h"
-	print ""	
-	
-	time.sleep(4)
-	UART.flushInput()
+        for Key in Datasets.keys():
+            if("GGA" in Key):
+                print("Use {} satellites".format(Datasets[Key][6]))
+                if(Datasets[Key][5] == "1"):
+                    print("Time: {}".format(Datasets[Key][0]))
+                    print("Latitude: {} - {}".format(Datasets[Key][1], Datasets[Key][2]))
+                    print("Longitude: {} - {}".format(Datasets[Key][3], Datasets[Key][4]))
+                else:
+                    print("GPS not fix!")
+                    print("GPS quality: {}".format(Datasets[Key][5]))
+            elif("GSV" in Key):
+                print("Number: {}".format(Datasets[Key][1]))
+                print("ID: {}".format(Datasets[Key][3]))
+                print("Visible satellites: {}".format(Datasets[Key][2]))
+                print("Elevation: {} degree".format(Datasets[Key][4]))
+                print("Azimuth: {} degree".format(Datasets[Key][5]))
+                print("SNR: {} dB".format(Datasets[Key][6]))
